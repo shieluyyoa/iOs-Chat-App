fbdb814a26f687e0e9a7ec7e7cf41c8b68f3f9c6
Oscarlui <oscarlu1@connect.hku.hk>
2022-04-08 02:10:18 +0800

    Initial Commit

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Oscar Lui on 8/4/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
